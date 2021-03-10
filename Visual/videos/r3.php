2094912ba4286fd2f8238465e80b01c5afb8fcdd
Beatriz Silva <79998297+Beatriz014@users.noreply.github.com>
2021-03-10 16:34:15 -0300

    Add files via upload

---
+++ Visual/videos/r3.php
@@ -0,0 +1,37 @@
+<?php
+  session_start();
+  if(!isset($_SESSION['user'])){
+    header("Location: ../login.php");
+  }else{
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Recompensa 3</title>
+	<link rel="stylesheet" type="text/css" href="../css/video.css">
+
+</head>
+<body>
+	<header>
+	<div class="row" align="center">
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" align="center" id="container" style="opacity: 0.9;">
+
+	<div class="col-xl-12 col-sm-4 col-md-8 col-lg-12" id="divVideo" align="center">
+		<h1>Recompensa 3</h1>
+		<div class="embed-responsive embed-responsive-21by9" align="center" id='idVideo'>
+		  <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/CmTsXyoWLPE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+		  <a href="../../final.php"><button> Voltar </button></a>
+		</div>
+	</div>
+</div>
+	</div>
+	</header>
+
+
+</body>
+</html>
+<?php
+}
+?>
